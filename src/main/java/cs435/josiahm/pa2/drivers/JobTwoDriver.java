2c3abe90e9b49dc0b1c44041860c6416f5118470
Josiah May <josiahm@rams.colostate.edu>
2018-10-02 12:43:36 -0600

    Attempt to us WriteComparable in Job1

--- src/main/java/cs435/josiahm/pa2/drivers/JobTwoDriver.java
+++ src/main/java/cs435/josiahm/pa2/drivers/JobTwoDriver.java
@@ -1,8 +1,10 @@
 package cs435.josiahm.pa2.drivers;
 
+import cs435.josiahm.pa2.mappers.JobTwoMapperIDF;
 import cs435.josiahm.pa2.mappers.JobTwoMapperTF;
 import cs435.josiahm.pa2.reducers.JobTwoReducer;
 import cs435.josiahm.pa2.writableComparables.StringDoubleValue;
+import cs435.josiahm.pa2.writableComparables.WordTermFrequency;
 import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -10,6 +12,9 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class JobTwoDriver {
@@ -17,13 +22,18 @@ public class JobTwoDriver {
   public static void main(String[] args)
       throws IOException, ClassNotFoundException, InterruptedException {
 
+    Path inputTF = new Path(args[0] + "/Job1/TF");
+    Path inputIDF = new Path(args[0] + "/Job1/IDF");
+
     // Setup job
     Configuration conf = new Configuration();
     Job job = Job.getInstance(conf, "Part 2, TF*IDF");
 
     // Setup Mapper
     job.setJarByClass(JobTwoDriver.class);
-    job.setMapperClass(JobTwoMapperTF.class);
+    MultipleInputs.addInputPath(job, inputTF, SequenceFileInputFormat.class, JobTwoMapperTF.class);
+    MultipleInputs.addInputPath(job, inputIDF, SequenceFileInputFormat.class, JobTwoMapperIDF.class);
+
 
     job.setMapOutputKeyClass(Text.class);
     job.setMapOutputValueClass(StringDoubleValue.class);
@@ -32,19 +42,10 @@ public class JobTwoDriver {
     job.setReducerClass(JobTwoReducer.class);
     job.setNumReduceTasks(10);
 
-    // Setup multiple outputs
-    /*
-    MultipleOutputs
-        .addNamedOutput(job, "TF", TextOutputFormat.class, Text.class, DoubleWritable.class);
-    MultipleOutputs
-        .addNamedOutput(job, "IDF", TextOutputFormat.class, Text.class, DoubleWritable.class);
-    */
-
     // Setup path arguments
-    Path input = new Path(args[0]);
-    Path output = new Path(args[1] + "/Job2/IDf*TF");
+    Path output = new Path(args[1] + "/Job2/IDF*TF");
 
-    FileInputFormat.addInputPath(job, input);
+
 
     // Remove output path if it exists
     FileSystem hdfs = FileSystem.get(conf);
--- src/main/java/cs435/josiahm/pa2/reducers/JobTwoReducer.java
+++ src/main/java/cs435/josiahm/pa2/reducers/JobTwoReducer.java
@@ -1,12 +1,49 @@
 package cs435.josiahm.pa2.reducers;
 
 import cs435.josiahm.pa2.writableComparables.StringDoubleValue;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class JobTwoReducer extends Reducer<Text, StringDoubleValue, Text, DoubleWritable> {
 
+  private final Text outputKey = new Text();
+  private final DoubleWritable outputValue = new DoubleWritable();
+  private final HashMap<String, Double> ids = new HashMap<>();
 
+  @Override
+  protected void reduce(Text key, Iterable<StringDoubleValue> values, Context context)
+      throws IOException, InterruptedException {
+
+    Double idfValue = new Double(0);
+
+
+    for (StringDoubleValue item: values) {
+
+      if(item.id != "-1")
+      {
+        ids.put(item.id, item.value);
+      } else {
+        idfValue = item.value;
+      }
+
+    }
+
+    if(idfValue == 0)
+    {
+      throw new RuntimeException("The IDF value for " + key.toString() + " is 0" );
+    }
+
+    for (Entry<String, Double> item: ids.entrySet()) {
+      outputKey.set(item.getKey() + key.toString());
+      outputValue.set(item.getValue());
+      context.write(outputKey, outputValue);
+    }
+
+    ids.clear();
+  }
 
 }
